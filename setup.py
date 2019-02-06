76904926cb477ddbf405b0084c497ee05a6b16be
zaq9 <zaq_9@yahoo.co.jp>
2019-02-07 00:00:02 +0900

    readme: change open

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from os import path
-from setuptools import setup
+from codecs import open
 
-here = path.abspath(path.dirname(__file__))
+from setuptools import setup, find_packages
 
-with open("README.md", "r",encoding="utf-8_sig") as fh:
-    long_description = fh.read()
+with open("README.md", "r", encoding="utf-8") as fh:
+	long_description = fh.read()
 
 setup(
     name='nk225op',
-    version='0.1.2',
+    version='0.1.5',
     description='Download latest Option Price(nk255) from JPX',
+    long_description=long_description,
     url='https://github.com/zaq9/nk225op',
     author='zaq',
     author_email='zaq_9@yahoo.co.jp',
